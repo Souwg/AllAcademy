88fd335ce052e06e1b247fedd243e3eaeba43fc5
Souwg <sansanvzla@gmail.com>
2026-02-17 19:01:08 +0000

    modificacion de variables de entorno en el archivo flux

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,4 +1,4 @@
-const API_BASE = process.env.BACKEND_URL;
+const API_BASE = process.env.REACT_APP_BACKEND_URL;
 
 const getState = ({ getStore, getActions, setStore }) => {
   // 🧰 Helpers internos reutilizables
